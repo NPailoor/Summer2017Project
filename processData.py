bc44fc218352817beedbf7211dba756741b57d8b
Shankara Pailoor <shankara.pailoor@echonous.com>
2017-08-07 14:40:51 -0700

    ProcessData updated

--- processData.py
+++ processData.py
@@ -3,6 +3,7 @@ import numpy
 from  matplotlib import pyplot as p
 from scipy import signal
 from caffe2.python import workspace
+import math
 
 def initialFiltering():
     conn = sqlite3.connect('test.db')
@@ -110,6 +111,25 @@ def eventParameters():
     cursor.close()
     conn.commit()
 
+def distanceParameters():
+    conn = sqlite3.connect('test.db')
+    print "Opened database successfully";
+    cursor = conn.cursor()
+    data = cursor.execute('''SELECT * FROM DATATABLE WHERE EVENT=1''')
+    data = data.fetchall()
+    for row in data:
+        tag = row[8]
+        rec = row[1]
+        lat1 = row[5]
+        lon1 = row[6]
+        (lat2, lon2) = stationCoordinates(rec)
+        d2rec = distance(lat1, lon1, lat2, lon2)
+        cursor.execute('''UPDATE DATATABLE SET d2rec=? WHERE TAG=?''', (d2rec, tag))
+        print("Updated Table successfully")
+    cursor.close()
+    conn.commit()
+    
+
 def createEventTable():
     conn = sqlite3.connect('test.db')
     print "Opened database successfully";
@@ -147,6 +167,51 @@ def medianAmp(ampLog):
     #p.show()
     return ampMedian
 
+def fixCoordinates():
+    import os
+    import scipy.io
+    conn = sqlite3.connect('test.db')
+    cursor = conn.cursor()
+    data = cursor.execute('''SELECT * FROM DATATABLE''')
+    data = data.fetchall()
+    for row in data:
+        date = row[3]
+        time = row[4]
+        tag = row[8]
+        path = '/home/nikhil/Desktop/geniza/Morris/EventsEF/'
+        dateString = str(date/10000) + '_' + str((date%10000)/100).zfill(2) + '_' + str(date%100).zfill(2)
+        fileString = str(date/10000) + '-' + str((date%10000)/100).zfill(2) + '-' + str(date%100).zfill(2) \
+                     + '-' + str(time/3600).zfill(2) + '-' +str((time%3600)/60).zfill(2) + '-' + str(time%60) \
+                     + '-' + str(abs(int(round(row[6]))));
+        fileStringalt = str(date/10000) + '-' + str((date%10000)/100).zfill(2) + '-' + str(date%100).zfill(2) \
+                     + '-' + str(time/3600).zfill(2) + '-' +str((time%3600)/60).zfill(2) + '-' + \
+                     str(time%60 - 1).zfill(2) + '-' + str(abs(int(round(row[6]))));
+        print dateString
+        print fileString
+        for i in  os.listdir(path + dateString):
+            if os.path.isfile(os.path.join(path + dateString,i)) and \
+               ((fileString in i) or (fileStringalt in i)):
+                print("successfully accessed file")
+                incident = scipy.io.loadmat(path + '/' + dateString + '/' + i)
+                info = incident['LightningInfo']
+                info = info[0]
+                lat = info[6]
+                lon = info[7]
+                peak = info[9]
+                cursor.execute('''UPDATE DATATABLE SET LAT=?, LONG=?, PEAK=? WHERE TAG=?''',\
+                               (lat, lon, peak, tag))
+                print("Table updated successfully")
+    cursor.close()
+    conn.commit()
+    
+    
+             
+            
+
+
+        
+    
+
 def normalizedAmp(ampMedian):
     #timeRange = list(range(-40,120))
     steadyState = numpy.median(ampMedian[20:40])
@@ -180,4 +245,72 @@ def dataTest(tag):
     p.subplot(414)
     p.plot(timeRange, dataNormal)
     p.show()
+
+def distance(lat1, long1, lat2, long2):
+    if (lat2 > 90):
+        return 0
+    lat1 = math.radians(lat1)
+    long1 = math.radians(long1)
+    lat2 = math.radians(lat2)
+    long2 = math.radians(long2)
+
+    angle = math.acos(math.sin(lat1)*math.sin(lat2) \
+                      + math.cos(lat1)*math.cos(lat2)*math.cos(long1-long2))
+    angle = math.degrees(angle)
+    #Distance in nautical miles
+    distance = 60.0 * angle
+    return distance
+
+def stationCoordinates(station):
+    if station == "NAA":
+        return(44 + (38/60) + (47.02/3600), -1*(67 + (16/60) + (51.85/3600)))
+    elif station == "NAU":
+        return (18.398775, -67.177486)
+    elif station == "NML":
+        return (46.365987, -98.335667)
+    elif station == "NPM":
+        return (-21.816325, 114.16546)
+    elif station == "NRK":
+        return (63.850365, -22.466773)
+    elif station == "DHO":
+        return (53.078900, 7.615000)
+    elif (station == "HWU" or station == "HW2" or station == "HW3"):
+        return (46.714119, 1.244309)
+    elif station == "NLK":
+        return (48.203633, 121.916828)
+    elif station == "GBZ":
+        return (52.72246, -3.06295)
+    elif station == "GQD":
+        return (54.911, -3.280)
+    elif station == "JJI":
+        return (32 + (4/60) + (36/3600), -1*(130 + (49/60) + (44/3600)))
+    elif station == "ICV":
+        return (40.923127, 9.731011)
+    elif station == "HWV":
+        return (48.544, 2.576)
+    elif station == "JXN":
+        return (59.910, 10.520)
+    elif station == "Burden":
+        return (37 + (18/60) + (51/3600), -1*(96 + (45/60) + (19/3600)))
+    elif station == "Baxley":
+        return (31 + (46/60) + (31/3600), -1*(82 + (20/60) + (51/3600)))
+    elif station == "Briarwood":
+        return (33 + (34/60) + (50/3600), -1*(84 + (26/60) + (37/3600)))
+    elif station == "Atlanta":
+        return (33 + (46/60) + (33/2600), -1*(84 + (23/60) + (41/3600)))
+    elif station == "Deleware":
+        return (39 + (9/60) + (29/3600), -1*(75 + (31/60) + (28/3600)))
+    elif station == "PARI":
+        return (35.1996, -82.8724)
+    elif station == "Toolik":
+        return (68 + (38/60), -1*(149 + (36/60)))
+    elif station == "Juneau":
+        return (58.3014485, -134.4216125)
+    else:
+        return (91,181)
+
+        
+    
+    
+
       
